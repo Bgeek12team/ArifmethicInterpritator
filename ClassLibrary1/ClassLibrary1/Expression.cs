61403f33391d1c1ec045c99f05aa833976b018ea
ghimik <bolit006.br0m@gmail.com>
2024-02-06 22:05:35 +0300

    created

---
+++ ClassLibrary1/ClassLibrary1/Expression.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibrary1
+{
+    public class Expression(string expression = null!)
+    {
+        private Token[]? Postfix { get; set; }
+        public string StringExpression { get; init; } = expression;
+
+        public double CalculateAt(params (char variable, double value)[] variables) 
+        {
+            if (Postfix == null)
+            {
+                try
+                {
+                    var tokens = Token.Tokenize(StringExpression);
+                    Postfix = Polish.ToInversePolishView(tokens);
+                } 
+                catch
+                {
+                    return double.NaN;
+                }
+            }
+
+            return CalculateInverse(Transform(Postfix, variables));
+        }
+
+        // вычислить из обратной записи значение
+        private double CalculateInverse(Token[] postfix)
+        {
+            throw new NotImplementedException();
+        }
+
+        // все переменные заменить в соответсвии с массивом туплей
+        private Token[] Transform(Token[] tokens, (char variable, double value)[] variables) 
+        { 
+             throw new NotImplementedException();
+        }
+    }
+
+}
